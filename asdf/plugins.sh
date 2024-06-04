b105fc87a5005d6ced3c9e054611915bb7f0c3b7
jonathan ng <j6n.work@gmail.com>
2024-06-04 12:12:12 -0400

    better git prompt, add asdf

---
+++ asdf/plugins.sh
@@ -0,0 +1,12 @@
+# languages
+asdf plugin add nodejs # https://github.com/asdf-vm/asdf-nodejs.git
+
+# infra
+asdf plugin add terraform # https://github.com/asdf-community/asdf-hashicorp.git
+asdf plugin add awscli # https://github.com/MetricMike/asdf-awscli.git
+asdf plugin-add aws-vault # https://github.com/karancode/asdf-aws-vault.git
+
+# k8s
+asdf plugin add talosctl # https://github.com/bjw-s/asdf-talosctl.git
+asdf plugin-add kubectl # https://github.com/asdf-community/asdf-kubectl.git
+asdf plugin-add k9s # https://github.com/looztra/asdf-k9s
