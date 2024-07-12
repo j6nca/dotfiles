400f6359dad7b69f09afcb32a17a6d372a7841d5
jonathan ng <jonathan.ng@stackadapt.com>
2024-07-12 09:25:31 -0400

    add kind for local testing

--- tools/asdf/setup.sh
+++ tools/asdf/setup.sh
@@ -8,14 +8,17 @@ asdf plugin add golang #https://github.com/asdf-community/asdf-golang.git
 
 # infra
 asdf plugin add terraform # https://github.com/asdf-community/asdf-hashicorp.git
-asdf plugin add awscli # https://github.com/MetricMike/asdf-awscli.git
+asdf plugin add awscli    # https://github.com/MetricMike/asdf-awscli.git
 asdf plugin-add aws-vault # https://github.com/karancode/asdf-aws-vault.git
 
 # k8s
-asdf plugin add talosctl # https://github.com/bjw-s/asdf-talosctl.git
-asdf plugin-add kubectl # https://github.com/asdf-community/asdf-kubectl.git
-asdf plugin-add k9s # https://github.com/looztra/asdf-k9s
-asdf plugin-add flux2 # https://github.com/tablexi/asdf-flux2.git
-asdf plugin-add fluxctl # https://github.com/stefansedich/asdf-fluxctl
-asdf plugin-add helm # https://github.com/Antiarchitect/asdf-helm.git
+asdf plugin add talosctl  # https://github.com/bjw-s/asdf-talosctl.git
+asdf plugin-add kubectl   # https://github.com/asdf-community/asdf-kubectl.git
+asdf plugin-add k9s       # https://github.com/looztra/asdf-k9s
+asdf plugin-add flux2     # https://github.com/tablexi/asdf-flux2.git
+asdf plugin-add fluxctl   # https://github.com/stefansedich/asdf-fluxctl
+asdf plugin-add helm      # https://github.com/Antiarchitect/asdf-helm.git
+asdf plugin-add kind      # https://github.com/johnlayton/asdf-kind.git
+
+
 
