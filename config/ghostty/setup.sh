db8d7c1e403e897f28106bb59655d24ce72f3dfc
jonathan ng <me@j6n.ca>
2025-05-14 17:50:18 -0400

    update setup scripts

--- config/ghostty/setup.sh
+++ config/ghostty/setup.sh
@@ -1,2 +1,3 @@
 mkdir -p ~/.config/ghostty
+[ -f ~/.config/ghostty/config ] && mv ~/.config/ghostty/config ~/.config/ghostty/config.backup
 ln -s ~/projects/dotfiles/config/ghostty/config ~/.config/ghostty/config
---
+++ config/git/setup.sh
@@ -0,0 +1,2 @@
+[ -f ~/.gitconfig ] && mv ~/.gitconfig ~/.gitconfig.backup
+ln -s ~/projects/dotfiles/config/git/config ~/.gitconfig
