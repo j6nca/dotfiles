2be02c1703bed1a6690c2c9cced17216b31dbbff
jonathan ng <me@j6n.ca>
2025-05-16 18:56:59 -0400

    add fish and kubectl

---
+++ setup/fish.sh
@@ -0,0 +1,7 @@
+brew install fish
+
+# Install package manager fisher
+curl -sL https://raw.githubusercontent.com/jorgebucaran/fisher/main/functions/fisher.fish | source && fisher install jorgebucaran/fisher
+
+# Install prompt customizer tide
+fisher install IlanCosman/tide@v6
