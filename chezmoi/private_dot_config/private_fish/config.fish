bff9bbfe49905b157bdbe6ff0bee26785f6ee80f
jonathan ng <me@j6n.ca>
2026-04-28 00:39:08 -0400

    add fishline

--- chezmoi/private_dot_config/private_fish/config.fish
+++ chezmoi/private_dot_config/private_fish/config.fish
@@ -3,4 +3,10 @@ if status is-interactive
 end
 eval "$(mise activate fish)"
 
-eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv fish)"
+# eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv fish)"
+
+# install fishline
+if status is-interactive
+    set FLINE_PATH $HOME/.config/fish/fishline
+    source $FLINE_PATH/init.fish
+end
